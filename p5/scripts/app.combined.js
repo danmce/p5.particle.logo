5045bae43f759678707a3dd147a5a7a55b52d42b
Daniel McEwen <daniel.mcewen@clusta.com>
2014-12-05 15:32:29 +0000

    Full code sample

--- p5/scripts/app.combined.js
+++ p5/scripts/app.combined.js
@@ -1,14 +1,80 @@
-var drawing = function (p5) {
-    var hintImage, skyImage, stars = [];
-    p5.setup = function () {
-        p5.createCanvas(1024, 768);
-        p5.noCursor();
-        p5.noStroke();
-        hintImage = p5.loadImage("/media/logo-clusta.jpg");
+var Color = (function () {
+    function Color(red, green, blue, alpha) {
+    }
+    return Color;
+})();
+var drawing = function (sketch) {
+    var limit = 500;
+    var hintImage;
+    var particles = [];
+    var inactiveFill = new Color(255, 255, 255, 1);
+    var inactiveStroke = new Color(120, 120, 120, 1);
+    var activeFill = new Color(255, 0, 0, 1);
+    var activeStroke = new Color(120, 0, 0, 1);
+    sketch.setup = function () {
+        sketch.createCanvas(1024, 768);
+        sketch.noCursor();
+        sketch.noStroke();
+        hintImage = sketch.loadImage("/media/logo-clusta.jpg");
     };
-    p5.draw = function () {
-        p5.background(0);
-        var position = p5.createVector(random(hintImage.width), random(hintImage.height));
+    sketch.draw = function () {
+        sketch.background(0);
+        var position = sketch.createVector(sketch.random(hintImage.width), sketch.random(hintImage.height));
+        sketch.ellipse(position.x, position.y, 8, 8);
+        var pixelProperties = constructPixelProperties();
+        var particle = new Particle(position, pixelProperties.target, pixelProperties.rgbaFill, pixelProperties.rgbaStroke);
+        particles.push(particle);
+        if (particles.length > limit)
+            particles.shift();
+        for (var i = 0; i < particles.length; i++) {
+            particles[i].update();
+            particles[i].draw();
+        }
+    };
+    function constructPixelProperties() {
+        var x, y, colorFill = inactiveFill, colorStroke = inactiveStroke;
+        for (var i = 0; i < 15; i++) {
+            x = sketch.floor(sketch.random(hintImage.width));
+            y = sketch.floor(sketch.random(hintImage.height));
+            if (sketch.red(hintImage.get(x, y)) < 255) {
+                colorFill = activeFill;
+                colorStroke = activeStroke;
+                break;
+            }
+        }
+        return {
+            target: sketch.createVector(x, y),
+            rgbaFill: colorFill,
+            rgbaStroke: colorStroke
+        };
+    }
+    function Particle(position, target, rgbaFill, rgbaStroke) {
+        this.position = position;
+        this.target = target;
+        this.diameter = sketch.random(1, 5);
+        this.fill = {
+            r: rgbaFill.red,
+            g: rgbaFill.green,
+            b: rgbaFill.blue,
+            a: rgbaFill.alpha
+        };
+        this.stroke = {
+            r: rgbaStroke.red,
+            g: rgbaStroke.green,
+            b: rgbaStroke.blue,
+            a: rgbaStroke.alpha
+        };
+    }
+    Particle.prototype.update = function () {
+        this.position = p5.Vector.lerp(this.position, this.target, 0.04);
+    };
+    Particle.prototype.draw = function () {
+        var alpha = sketch.noise(this.target.x, this.target.y, sketch.millis() / 1000.0);
+        sketch.fill(this.fill.r, this.fill.g, this.fill.b, alpha * 255);
+        var s = particles[1];
+        sketch.line(this.position.x, this.position.y, s.position.x, sketch.random(hintImage.width));
+        sketch.stroke(this.stroke.r, this.stroke.g, this.stroke.b, alpha * 255);
+        sketch.ellipse(this.position.x, this.position.y, this.diameter, this.diameter);
     };
 };
 var canvas = new p5(drawing);
--- p5/scripts/typings/p5/p5.d.ts
+++ p5/scripts/typings/p5/p5.d.ts
@@ -1,10 +1,12 @@
-﻿declare module _p5 {
+﻿declare module p5 {
 
     export interface instance {
         new (s: (context: context) => void);
+        Vector : Vector;
     }
 
-    export class context {
+    export interface context {
+
         setup(): any;
         draw(): any;
 
@@ -13,21 +15,46 @@
         noStroke(): void;
 
         loadImage(image: string): Image;
-        createVector(x: number, y: number): void;
+        createVector(x: number, y: number): Vector;
+
+        random(min: number, max?: number): number;
 
         background(grayValue: number): void;
         background(color: string): void;
         background(red: number, green: number, blue: number, opacity?: number): void;
         background(hue: number, saturation: string[], brightness: string[], opacity: string[]): void;
+
+        //Calculation
+        floor(n: number): number;
+
+        //Random
+        random(min: number, max: number): number;
+
+        //Shape
+        ellipse(x: number, y: number, width: number, height: number): void;
+
+        //Color
+        red(obj: any): number;
+
+        //Noise
+        noise();
     }
 
-    interface Image {
+    export interface Image {
         width: number;
         height: number;
         pixels: number[];
         loadPixels(): number[];
+        get(): any;
+        get(x: number, y: number): any;
     }
 
+    export interface Vector {
+        x: number;
+        y: number;
+        z: number;
+        lerp(x: any, y?: any, z?: any, amt?: any): void;
+    }
 }
 
-declare var p5: _p5.instance;
+declare var p5: p5.instance;
